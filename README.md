45a7dc05a7795191fa7320c13973cc0a861a4353
stevenmalley <steven.malley@gmail.com>
2022-03-30 21:29:53 +0100

    dril info added to README, script file containing example tweet

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # random tweet generator
 
+in the style of @dril tweets.
+
 runs with Node.js.
 
 should be made up of at least three different pieces of data.
---
+++ script.js
@@ -0,0 +1,5 @@
+const components = {
+    1: ["james bond the good fellas and god father"],
+    2: ["team up to shoot"],
+    3: ["the most ufos you have everf fucking seen"]
+}
